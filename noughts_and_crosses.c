6cc785455a14f6aefa67ec8d0af237e867a7fbd8
csaz3507 <mikael.di-re@student.uibk.ac.at>
2021-05-03 13:27:09 +0200

    Noughts and crosses

---
+++ noughts_and_crosses.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/*0 0 0
+  0 0 0
+  0 0 0
+*/
+
+void print_board(const int *board) 
+	for (int i = 0; i < 9; i++) {
+		printf("%d %d %d
+}
+
+int main() {
+
+	int board[9] = {0};
+	
+	const int NOUGHTS = 1;
+	const int CROSSES = 2;
+	const int EMPTY = 0;
+
+	return 0;
+}

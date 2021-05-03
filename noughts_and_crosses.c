c65cfb02f14ebc8c7c3199d335a0d5cb04a4b6f7
csaz3507 <mikael.di-re@student.uibk.ac.at>
2021-05-03 13:40:51 +0200

    input added

--- noughts_and_crosses.c
+++ noughts_and_crosses.c
@@ -1,13 +1,43 @@
 #include <stdio.h>
 
-/*0 0 0
-  0 0 0
-  0 0 0
-*/
 
-void print_board(const int *board) 
-	for (int i = 0; i < 9; i++) {
-		printf("%d %d %d
+
+void print_board(const int *board) {
+}
+
+int check_win(/*board??*/) {
+}
+
+void place_X () {
+	
+	int pos_X;
+	
+	printf("Choose a position from 1-9 to place your X.");
+	
+	scanf("%d", &pos_X);
+	
+	if (board[pos_X-1] != 0) {
+		printf("This position has already been filled, please try another.");
+		place_X();
+	}
+	
+	board[pos_X-1] = 1;
+}
+
+void place_O () {
+	
+	int pos_O;
+	
+	printf("Choose a position from 1-9 to place your O.");
+	
+	scanf("%d", &pos_O);
+	
+	if (board[pos_O-1] != 0) {
+		printf("This position has already been filled, please try another.");
+		place_O();
+	}
+	
+	board[pos_O-1] = 1;
 }
 
 int main() {
@@ -17,6 +47,8 @@ int main() {
 	const int NOUGHTS = 1;
 	const int CROSSES = 2;
 	const int EMPTY = 0;
+	
+	
 
 	return 0;
 }

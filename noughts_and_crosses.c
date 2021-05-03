1f8e448f765615522f8b7dc9df8df6cca363d72e
csaz3507 <mikael.di-re@student.uibk.ac.at>
2021-05-03 18:52:09 +0200

    game over checks add(ish)

--- noughts_and_crosses.c
+++ noughts_and_crosses.c
@@ -1,54 +1,107 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 
+//function declaration
+void print_board(int *board);
+int three_in_row(int *board);
+int is_draw(int *board);
+int *place_X(int *board);
+int *place_O(int *board);
 
-void print_board(const int *board) {
-}
-
-int check_win(/*board??*/) {
-}
-
-void place_X () {
+const int NOUGHTS = 1;
+const int CROSSES = 2;
+const int EMPTY = 0;
 	
-	int pos_X;
+int main() {
+	//board initialised to 0s
+	int board[9] = {0}
 	
-	printf("Choose a position from 1-9 to place your X.");
+	printf("\n");
+	print_board(board);
 	
-	scanf("%d", &pos_X);
-	
-	if (board[pos_X-1] != 0) {
-		printf("This position has already been filled, please try another.");
-		place_X();
+	//play while not a win or draw
+	while (!three_in_row(board) && !is_draw(board)) {
+
 	}
 	
-	board[pos_X-1] = 1;
+	if (three_in_row) {
+		printf("Game over. %d has won!", winner);
+	} else {
+		printf("Game over. It's a draw!);
+	}
+	
+	return 0;
 }
 
-void place_O () {
+int *place_O(int *board) {
 	
 	int pos_O;
 	
-	printf("Choose a position from 1-9 to place your O.");
+	printf("Choose a position from 1-9 to place your O.\n");
 	
 	scanf("%d", &pos_O);
 	
 	if (board[pos_O-1] != 0) {
-		printf("This position has already been filled, please try another.");
-		place_O();
+		printf("This position has already been filled, please try another.\n");
+		place_O(board);
+	} else {
+		board[pos_O-1] = NOUGHTS;
 	}
-	
-	board[pos_O-1] = 1;
+	return board;
 }
 
-int main() {
+int *place_X(int *board) {
+	
+	int pos_X;
+	
+	printf("Choose a position from 1-9 to place your X.\n");
+	
+	scanf("%d", &pos_X);
+	
+	if (board[pos_X-1] != 0) {
+		printf("This position has already been filled, please try another.\n");
+		place_X(board);
+	} else {
+		board[pos_X-1] = CROSSES;
+	}
+	return board;
+}
 
-	int board[9] = {0};
+void print_board(int *board) {
 	
-	const int NOUGHTS = 1;
-	const int CROSSES = 2;
-	const int EMPTY = 0;
-	
-	
+	for (int i = 0; i < 9; i++) {
+		printf("%d ", board[i]);
+		if ((i+1)%3 == 0)
+			printf("\n");
+	}
+	printf("\n");	
+}
 
-	return 0;
+int three_in_row(int *board) {
+	
+	int flag = 0;
+	
+	for (int i = 0; i <= 6; i + 3) {
+		if (board[i] == board[i+1] && board[i] == board[i+2]) {
+			flag = 1;
+		}
+	}
+	
+	for (int i = 0; i < 3; i++) {
+		if (board[i] == board[i+3] && board[i] == board[i+6]) {
+			flag = 1;
+		}
+	}
+	
+	if (board[0] == board[4] && board[0] == board[8])	flag = 1;
+	if (board[2] == board[4] && board[2] == board[6])	flag = 1;
+	
+	return flag;
+}
+
+int is_draw(int *board) {
+	for (int i = 0; i < 9; i++) {
+		if (board[i] == EMPTY)	return 0;
+	}
 }
